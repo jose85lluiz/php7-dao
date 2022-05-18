89d145fc9e808b096477340994088b25d79fe784
joseluiz <jose--luiz@hotmail.com>
2022-05-18 16:49:17 -0300

    Novos metodos na classe Usuario

--- Class/Usuario.php
+++ Class/Usuario.php
@@ -42,7 +42,7 @@ class Usuario{
 	        $this -> dtcadastro = $value;
 		}
 
-public function loadById($id){
+public function loadById($id){        // metodo para mostrar apenas 1 usuario do banco de dados, pelo id informado no index
 $sql = new Sql();
 $result = $sql -> select("SELECT * FROM tb_usuarios WHERE id_usuario =:ID",array(
 
@@ -58,13 +58,48 @@ if (isset($result[0])){
     }
   }
 
+  public static function getList(){   // metodo para listar usuarios do banco de dados , ordem por id
+  	$sql = new Sql();
+  	return $sql -> select("SELECT * FROM tb_usuarios ORDER BY id_usuario;");
+
+  }
+  public static function search($login){// metodo para procurar dados no banco de dados .
+  	$sql = new Sql();
+     return $sql -> select("SELECT * FROM  tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin",array(
+     ':SEARCH' => "%".$login."%" 
+    
+     ));
+
+  }
+
+public function login($login,$pass){
+$sql = new Sql();
+$result = $sql -> select("SELECT * FROM tb_usuarios WHERE deslogin =:LOGIN AND dessenha = :PASSWORD",array(
+
+":LOGIN"=> $login,
+":PASSWORD"=> $pass));
+
+if (isset($result[0])){
+
+	$row = $result[0];
+	$this -> setId_usuario($row['id_usuario']);
+	$this -> setDeslogin($row['deslogin']);
+	$this -> setDessenha($row['dessenha']);
+	$this -> setDtcadastro(new DateTime($row['dtcadastro']));
+    } else {
+     throw new exception("Login e/ou senha inválidos"); 
+
+    }
+
+}
+
 public function __toString(){
 
  return json_encode(array(
  "id_usuario"=> $this -> getId_usuario(),
  "deslogin" => $this -> getDeslogin(),
  "dessenha" => $this -> getDessenha(),
- "dtcadastro"=> $this -> getDtcadastro() -> format("d/m/y H:i:s")
+ "dtcadastro"=> $this -> getDtcadastro() -> format("d .m. Y :: H:i:s")
 
  ));
 }
--- index.php
+++ index.php
@@ -1,10 +1,27 @@
 <?php
 
 require_once("config.php");
-$root = new Usuario();
+$root = new Usuario(); 
+$root -> loadById(1); // informar o id 
+echo "MOSTRA APENAS UM CADASTRO :</br>";
+echo $root; // Mostrar apenas um usuario do banco de dados.
 
-$root -> loadById(2);
+echo "</br>";
+echo "</br>";
+echo "LISTA POR ORDEM DO ID:</br>";
+echo "-------------------------------------------------------------------------------------------------------------------------------</br>";
+$lista = Usuario::getList();//chamando metodo estatico 'getList()' na classe usuario. Para listar dados do banco de dados.
+echo json_encode($lista);
+echo "</br>";
+echo "-------------------------------------------------------------------------------------------------------------------------------</br>";
+echo "POR BUSCA:</br>";
+$busca = Usuario::search("jo");
+echo json_encode($busca);
+echo "</br>";
 
-echo $root;
+echo "SE LOGIN E SENHA FOREM VALIDAS:</br>";
+$user = new Usuario();
+$user -> login ("josluiz","774433");
 
+echo $user;
 ?>
